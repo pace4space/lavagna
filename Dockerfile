cef68e660cd27a4ebdd42ddb94043c1e41a99250
IC <pace4space@gmail.com>
2026-03-16 15:28:44 +0200

    exec in sh; mvn clean early step

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,17 @@ FROM maven:3.6.3-openjdk-8
 
 EXPOSE 8080
 
+# Build from project files
+COPY . /app
+WORKDIR /app
+RUN mvn clean package -DskipTests
+
 # Use local file as database - who cares
 ENV DB_DIALECT HSQLDB
 ENV DB_URL jdbc:hsqldb:file:lavagna
 ENV DB_USER sa
 ENV DB_PASS ""
 ENV SPRING_PROFILE dev
-
-# Build from lavagna project
-COPY . /app
-WORKDIR /app
-RUN mvn clean package
 
 # Execute the web archive
 COPY entrypoint.sh .
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
--Ddatasource.url="${DB_URL}" \
--Ddatasource.username="${DB_USER}" \
--Ddatasource.password="${DB_PASS}" \
--Dspring.profiles.active="${SPRING_PROFILE}" \
--jar ./lavagna/lavagna/lavagna-jetty-console.war --headless
+exec java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
+  -Ddatasource.url="${DB_URL}" \
+  -Ddatasource.username="${DB_USER}" \
+  -Ddatasource.password="${DB_PASS}" \
+  -Dspring.profiles.active="${SPRING_PROFILE}" \
+  -jar ./lavagna/lavagna/lavagna-jetty-console.war --headless
